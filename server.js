f3836d5e75ee936f021327f26a2609d2e2ae0cdd
Xpectedlol <JOjooakpark@gmail.com>
2025-05-24 21:55:09 -0500

    connections

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const today = require("./api/wordle")
+const today = require("./api/wordle");
+const connections = require("./api/connections");
 const app = express();
 const port = 3001;
 
@@ -8,11 +9,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/today", today);
+app.use("/api/connections", connections);
 
 app.use("/", express.static("public"));
 
 app.get("/answers/wordle/", (req, res) => {
     res.sendFile(__dirname + "/public/answers/wordle.html");
+});
+
+app.get("/answers/connections/", (req, res) => {
+    res.sendFile(__dirname + "/public/answers/connections.html");
 });
 
 app.listen(port, () => {
